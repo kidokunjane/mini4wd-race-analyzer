0a9f0316ee4958bb95db757cd0652c16ee2cf0e6
kidokeita <yoyobaka@gmail.com>
2025-08-15 12:01:28 +0900

    バージョン更新

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // Centralized app version (used by UI and Service Worker)
 // Access via window.__APP_VERSION__ (UI) and self.__APP_VERSION__ (SW)
-self.__APP_VERSION__ = '0.0.3';
+self.__APP_VERSION__ = '0.0.4';
 
